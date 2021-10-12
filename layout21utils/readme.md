b25fa8b955590120fb8b22827f219f4a06e90193
Dan Fritchman <dan@fritch.mn>
2021-10-11 22:15:04 -0700

    Gds, Lef, Utils v0.2 Publishing

---
+++ layout21utils/readme.md
@@ -0,0 +1,7 @@
+# layout21utils
+
+
+## Layout21 Internal Utilities Crate
+
+
+License: BSD-3-Clause
--- layout21utils/src/lib.rs
+++ layout21utils/src/lib.rs
@@ -2,12 +2,14 @@
 //! # Layout21 Internal Utilities Crate
 //!
 
-// Internal Modules
 pub mod ptr;
 pub use ptr::*;
+
 pub mod ser;
 pub use ser::*;
+
 pub mod error;
 pub use error::*;
+
 pub mod dep_order;
 pub use dep_order::*;
